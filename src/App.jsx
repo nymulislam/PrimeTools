76201515895c527d0a76d56047580a8aa4624da1
nymulislam <naymulislam241@gmail.com>
2026-04-08 21:10:13 +0600

    feat: initialize project with Vite, React, and TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Nav from './components/Nav/Nav'
+function App() {
+
+  return (
+    <>
+      <div className='container mx-auto'>
+      <Nav></Nav>
+      </div>
+    </>
+  )
+}
+
+export default App
